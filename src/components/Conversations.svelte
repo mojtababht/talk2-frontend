94b932e482509446e439c8635f6cfcab0b172caa
mojtaba <mojtababht@gmail.com>
2024-08-17 21:40:43 +0330

    fixed each block

--- src/components/Conversations.svelte
+++ src/components/Conversations.svelte
@@ -1,5 +1,6 @@
 <script>
     import {onMount} from "svelte";
+    import avatar from "$lib/avatar.jpg"
 
     export let access_token
     const backend_base_url = 'http://127.0.0.1:8000/'
@@ -16,10 +17,9 @@
             })
         if (response.ok) {
             chats = await response.json()
+            chats = chats.results
         }
     })
-
-
 
 
 </script>
@@ -28,7 +28,7 @@
 <div class="conversation-area">
     {#each chats as chat}
         <div class="msg online">
-            <img class="msg-profile" src={chat.avatar} alt={chat.name}/>/ />
+            <img class="msg-profile" src={chat.avatar ? chat.avatar : avatar} alt={chat.name}/>
             <div class="msg-detail">
                 <div class="msg-username">{chat.name}</div>
                 <div class="msg-content">
@@ -38,31 +38,31 @@
             </div>
         </div>
     {/each}
-    <div class="msg">
-        <img class="msg-profile" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%2812%29.png" alt="" />
-        <div class="msg-detail">
-            <div class="msg-username">Miguel Cohen</div>
-            <div class="msg-content">
-                <span class="msg-message">Adaptogen taiyaki austin jean shorts brunch</span>
-                <span class="msg-date">20m</span>
-            </div>
-        </div>
-    </div>
-    <div class="msg active">
-        <div class="msg-profile group">
-            <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
-                <path d="M12 2l10 6.5v7L12 22 2 15.5v-7L12 2zM12 22v-6.5" />
-                <path d="M22 8.5l-10 7-10-7" />
-                <path d="M2 15.5l10-7 10 7M12 2v6.5" /></svg>
-        </div>
-        <div class="msg-detail">
-            <div class="msg-username">CodePen Group</div>
-            <div class="msg-content">
-                <span class="msg-message">Aysenur: I love CSS</span>
-                <span class="msg-date">28m</span>
-            </div>
-        </div>
-    </div>
+<!--    <div class="msg">-->
+<!--        <img class="msg-profile" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%2812%29.png" alt="" />-->
+<!--        <div class="msg-detail">-->
+<!--            <div class="msg-username">Miguel Cohen</div>-->
+<!--            <div class="msg-content">-->
+<!--                <span class="msg-message">Adaptogen taiyaki austin jean shorts brunch</span>-->
+<!--                <span class="msg-date">20m</span>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="msg active">-->
+<!--        <div class="msg-profile group">-->
+<!--            <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">-->
+<!--                <path d="M12 2l10 6.5v7L12 22 2 15.5v-7L12 2zM12 22v-6.5" />-->
+<!--                <path d="M22 8.5l-10 7-10-7" />-->
+<!--                <path d="M2 15.5l10-7 10 7M12 2v6.5" /></svg>-->
+<!--        </div>-->
+<!--        <div class="msg-detail">-->
+<!--            <div class="msg-username">CodePen Group</div>-->
+<!--            <div class="msg-content">-->
+<!--                <span class="msg-message">Aysenur: I love CSS</span>-->
+<!--                <span class="msg-date">28m</span>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
     <button class="add"></button>
     <div class="overlay"></div>
 </div>
